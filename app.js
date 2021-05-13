a67ee441c8ce5fe680b3d59b1bcd59943cf7a5a6
Ishani2010 <83572032+Ishani2010@users.noreply.github.com>
2021-05-13 22:26:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const Judwaa2Button = document.getElementById ("Judwaa-2");
+Judwaa2Button.onclick=()=>{
+    location.href = "./Judwaa2.html";
+};
+
+const GolmaalAgainButton = document.getElementById ("Golmaal-Again");
+GolmaalAgainButton.onclick=()=>{
+    location.href = "./GolmaalAgain.html";
+}; 
+
+const CocoButton = document.getElementById ("Coco");
+CocoButton.onclick=()=>{
+    location.href = "./Coco.html";
+}; 
+
+const AladdinButton = document.getElementById ("Aladdin-Real-Life");
+AladdinButton.onclick=()=>{
+    location.href = "./AladdinRealLife.html";
+}; 
+
